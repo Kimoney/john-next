60c4f5d2dffd60db3305be09034be5e56faa2bf5
John <johntaykimani@gmail.com>
2024-07-18 05:06:40 +0300

    style: Updated color theme.

--- src/app/ui/navbar.jsx
+++ src/app/ui/navbar.jsx
@@ -7,22 +7,22 @@ export default function Navbar() {
     <>
     {/* Medium Devices and Above*/}
     <div className="hidden md:block text-center text-2xl pt-4 space-x-12">
-    <Link className="inline-block md:inline" href="/"> Home</Link>
-    <Link className="inline-block md:inline" href="/services"> Services</Link>
-    <Link className="inline-block md:inline" href="/portfolio"> Portfolio</Link>
-    <Link className="inline-block md:inline" href="/about"> About</Link>
-    <Link className="inline-block md:inline" href="/contact"> Contact</Link>
+    <Link className="inline-block md:inline hover:text-orange-400" href="/"> Home</Link>
+    <Link className="inline-block md:inline hover:text-orange-400" href="/services"> Services</Link>
+    <Link className="inline-block md:inline hover:text-orange-400" href="/portfolio"> Portfolio</Link>
+    <Link className="inline-block md:inline hover:text-orange-400" href="/about"> About</Link>
+    <Link className="inline-block md:inline hover:text-orange-400" href="/contact"> Contact</Link>
     </div>
     
     {/* Mobile Devices View */}
     
     <div className="md:hidden flex place-content-around text-3xl pt-3 space-x-6">
 
-    <Link className="md:hidden" href="/"> <FaHouseUser/> </Link>
-    <Link className="md:hidden" href="/services"> <MdEngineering/> </Link>
-    <Link className="md:hidden" href="/portfolio"> <MdCollectionsBookmark/> </Link>
-    <Link className="md:hidden" href="/about"> <FaCircleInfo /> </Link>
-    <Link className="md:hidden" href="/contact"> <FaPhone/> </Link>
+    <Link className="md:hidden hover:text-orange-400" href="/"> <FaHouseUser/> </Link>
+    <Link className="md:hidden hover:text-orange-400" href="/services"> <MdEngineering/> </Link>
+    <Link className="md:hidden hover:text-orange-400" href="/portfolio"> <MdCollectionsBookmark/> </Link>
+    <Link className="md:hidden hover:text-orange-400" href="/about"> <FaCircleInfo /> </Link>
+    <Link className="md:hidden hover:text-orange-400" href="/contact"> <FaPhone/> </Link>
     </div>
     </>
   )
