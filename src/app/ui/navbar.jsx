aea8247a3178b3d3ca7c206911dcbead7498b42d
John <johntaykimani@gmail.com>
2024-07-18 05:42:11 +0300

    feat: Implemented usePathname on Navbar to ensure update on active link.

--- src/app/ui/navbar.jsx
+++ src/app/ui/navbar.jsx
@@ -1,17 +1,22 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from "next/navigation";
 import { FaHouseUser, FaPhone, FaCircleInfo } from "react-icons/fa6";
 import { MdCollectionsBookmark, MdEngineering } from "react-icons/md";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <>
     {/* Medium Devices and Above*/}
     <div className="hidden md:block text-center text-2xl pt-4 space-x-12">
-    <Link className="inline-block md:inline hover:text-orange-400" href="/"> Home</Link>
-    <Link className="inline-block md:inline hover:text-orange-400" href="/services"> Services</Link>
-    <Link className="inline-block md:inline hover:text-orange-400" href="/portfolio"> Portfolio</Link>
-    <Link className="inline-block md:inline hover:text-orange-400" href="/about"> About</Link>
-    <Link className="inline-block md:inline hover:text-orange-400" href="/contact"> Contact</Link>
+    <Link href="/" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/' ? 'text-orange-500' : ''}`}> Home</Link>
+    <Link href="/services" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/services' ? 'text-orange-500' : ''}`}> Services</Link>
+    <Link href="/portfolio" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/portfolio' ? 'text-orange-500' : ''}`}> Portfolio</Link>
+    <Link href="/about" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/about' ? 'text-orange-500' : ''}`}> About</Link>
+    <Link href="/contact" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/contact' ? 'text-orange-500' : ''}`}> Contact</Link>
     </div>
     
     {/* Mobile Devices View */}
