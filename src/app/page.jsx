60e851bab5b86722cae0951c46386702da29f296
John <johntaykimani@gmail.com>
2024-07-19 17:17:20 +0300

    style: Fixed heading positions and size.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
   <Navbar />
-  <div className="flex flex-col items-center justify-center min-h-screen">
-    <div className="relative w-64 h-64 rounded-full overflow-hidden border-4 border-gray-700">
+  <div className="flex flex-col items-center justify-center min-h-screen px-4">
+    <div className="relative w-64 h-64 rounded-full overflow-hidden border-4 border-orange-500 -mt-56">
       <Image
         className="object-cover"
         src="https://images.pexels.com/photos/13919082/pexels-photo-13919082.jpeg"
@@ -16,15 +16,19 @@ export default function Home() {
         // placeholder="blur" // Optional blur-up while loading
       />
     </div>
-    <h1 className="text-6xl text-center mt-6 font-extrabold">
-      Hi, I'm <span className="text-orange-500">John Kimani</span>
-    </h1>
-    <h1 className="text-3xl text-center mt-2 font-bold">
-      I'm a <span className="text-orange-500">FullStack Developer</span>
-    </h1>
-    <h2 className="text-center mt-4 px-4">
-      From concept to completion, I provide clean code and pixel-perfect designs for outstanding websites. Proficient in Next.js, React, Flask, and Django, I enhance user engagement with interactive web animations. My experience in customer relationship management ensures that every project is user-centric and impactful.
-    </h2>
+    <div className="text-left mt-16">
+      <h1 className="text-6xl font-extrabold">
+        Hi, I am <span className="text-orange-500">John Kimani</span>.
+      </h1>
+      <h1 className="text-3xl mt-6 font-bold">
+        A seasoned <span className="text-orange-500">FullStack Developer</span>.
+      </h1>
+      <h2 className="text-lg mt-6 px-2">
+        From concept to completion, I provide clean code and pixel-perfect designs for outstanding websites.
+        <br />
+        My experience in customer relationship management ensures that every project is <span className="text-orange-500 font-bold">user-centric</span> and <span className="text-orange-500 font-bold">impactful</span>.
+      </h2>
+    </div>
   </div>
 </>
 
