8473b6bfd916e0c7a3a4f6916f3b6e7d034efcd4
John <johntaykimani@gmail.com>
2024-07-19 19:18:49 +0300

    style: Fixed margin on home elements.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen px-4">
         <motion.div
-          className="relative w-64 h-64 rounded-full overflow-hidden border-4 border-orange-500"
+          className="relative w-64 h-64 rounded-full overflow-hidden border-4 border-orange-500 -mt-56"
           initial={{ opacity: 0, y: 50 }} // Start from 50px below
           animate={{ opacity: 1, y: 0 }} // Animate to original position
           transition={{ duration: 0.1 }} // Duration of the animation
@@ -22,12 +22,12 @@ export default function Home() {
             layout="fill"
           />
         </motion.div>
-        <div className="text-left mt-32">
+        <div className="text-left mt-12">
           <motion.h1
           className="text-4xl md:text-6xl font-extrabold"
           initial={{ opacity: 0, y: 50 }} // Start from 50px below
           animate={{ opacity: 1, y: 0 }} // Animate to original position
-          transition={{ duration: 0.3, delay: 0.2 }} // Duration and delay of the animation
+          transition={{ duration: 0.4, delay: 0.2 }} // Duration and delay of the animation
           >
             Hi, I am <span className="text-orange-500">John Kimani</span>.
           </motion.h1>
@@ -35,15 +35,15 @@ export default function Home() {
           className="text-2xl md:text-3xl mt-4 font-bold"
           initial={{ opacity: 0, y: 50 }} // Start from 50px below
           animate={{ opacity: 1, y: 0 }} // Animate to original position
-          transition={{ duration: 0.3, delay: 0.6 }} // Duration and delay of the animation
+          transition={{ duration: 0.4, delay: 0.7 }} // Duration and delay of the animation
           >
             A seasoned <span className="text-orange-500">FullStack Developer</span>.
           </motion.h1>
           <motion.h2
-          className="text-base md:text-lg mt-4 px-4"
+          className="text-base md:text-lg mt-4"
           initial={{ opacity: 0, y: 50 }} // Start from 50px below
           animate={{ opacity: 1, y: 0 }} // Animate to original position
-          transition={{ duration: 0.3, delay: 0.9 }} // Duration and delay of the animation
+          transition={{ duration: 0.4, delay: 1.2 }} // Duration and delay of the animation
           >
             From concept to completion, I provide clean code and pixel-perfect designs for outstanding websites.
             <br />
