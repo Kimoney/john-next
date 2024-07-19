8b47d6c26b40c1ac582a4c55119cc78ff4f7e1ac
John <johntaykimani@gmail.com>
2024-07-19 19:53:15 +0300

    style: Styled footer.

--- src/app/ui/footer.jsx
+++ src/app/ui/footer.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 export default function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
   return (
     <footer className="py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
         {/* Text on the left */}
         <p className="text-sm md:text-base text-gray-700">
-          © 2024 John Kimani Muchina. All rights reserved.
+          © {currentYear} John Kimani Muchina. All rights reserved.
         </p>
         {/* Icons on the right */}
-        <div className="flex space-x-4">
+        <div className="flex text-base md:text-lg space-x-4">
           <a href="https://github.com/Kimoney" target="_blank" rel="noopener noreferrer">
             <FaGithub className="text-gray-700 hover:text-orange-500 transition-colors" />
           </a>
