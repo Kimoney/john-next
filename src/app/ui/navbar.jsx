83a8d921b7cc5c01671e2f83193582f432b0944e
John <johntaykimani@gmail.com>
2024-07-19 20:01:10 +0300

    feat: Added Opacity in Navbar.

--- src/app/ui/navbar.jsx
+++ src/app/ui/navbar.jsx
@@ -10,34 +10,33 @@ export default function Navbar() {
 
   return (
     <>
-    {/* Medium Devices and Above*/}
-  <div className="hidden md:block sticky top-0">
-  <div className="flex justify-center">
-    <button className="inline-block md:inline border-solid rounded-2xl border border-gray-700 bg-gray-950 text-xl p-4 mt-6 space-x-12">
-    <Link href="/" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/' ? 'text-orange-500' : ''}`}> Home</Link>
-    <Link href="/services" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/services' ? 'text-orange-500' : ''}`}> Services</Link>
-    <Link href="/portfolio" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/portfolio' ? 'text-orange-500' : ''}`}> Portfolio</Link>
-    <Link href="/about" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/about' ? 'text-orange-500' : ''}`}> About</Link>
-    <Link href="/contact" className={`inline-block md:inline hover:text-orange-400 ${pathname === '/contact' ? 'text-orange-500' : ''}`}> Contact</Link>
-    </button>
-    </div>
-  </div>  
-    {/* Mobile Devices View */}
-    
-    <div className="md:hidden sticky top-0">
-    <div className="flex flex-row justify-center">
+      {/* Medium Devices and Above */}
+      <div className="hidden md:block sticky top-0 z-50">
+        <div className="flex justify-center">
+          <button className="bg-opacity-75 inline-block border-solid rounded-2xl border border-gray-700 bg-gray-950 text-xl p-4 mt-6 space-x-12">
+            <Link href="/" className={`inline-block hover:text-orange-400 ${pathname === '/' ? 'text-orange-500' : ''}`}> Home</Link>
+            <Link href="/services" className={`inline-block hover:text-orange-400 ${pathname === '/services' ? 'text-orange-500' : ''}`}> Services</Link>
+            <Link href="/portfolio" className={`inline-block hover:text-orange-400 ${pathname === '/portfolio' ? 'text-orange-500' : ''}`}> Portfolio</Link>
+            <Link href="/about" className={`inline-block hover:text-orange-400 ${pathname === '/about' ? 'text-orange-500' : ''}`}> About</Link>
+            <Link href="/contact" className={`inline-block hover:text-orange-400 ${pathname === '/contact' ? 'text-orange-500' : ''}`}> Contact</Link>
+          </button>
+        </div>
+      </div>
 
-    <button className="md:hidden border-solid rounded-2xl border border-gray-700 bg-gray-950 text-xl p-4 mt-6">
-    <div className="flex space-x-16">
-    <Link href="/" className={`md:hidden hover:text-orange-400 ${pathname === '/' ? 'text-orange-500' : ''}`}> <FaHouseUser/> </Link>
-    <Link href="/services" className={`md:hidden hover:text-orange-400 ${pathname === '/services' ? 'text-orange-500' : ''}`}> <MdEngineering/> </Link>
-    <Link href="/portfolio" className={`md:hidden hover:text-orange-400 ${pathname === '/portfolio' ? 'text-orange-500' : ''}`}> <MdCollectionsBookmark/> </Link>
-    <Link href="/about" className={`md:hidden hover:text-orange-400 ${pathname === '/about' ? 'text-orange-500' : ''}`}> <FaCircleInfo /> </Link>
-    <Link href="/contact" className={`md:hidden hover:text-orange-400 ${pathname === '/contact' ? 'text-orange-500' : ''}`}> <FaPhone/> </Link>
-    </div>
-    </button>
-    </div>
-    </div>
+      {/* Mobile Devices View */}
+      <div className="md:hidden sticky top-0 z-50">
+        <div className="flex flex-row justify-center">
+          <button className="bg-opacity-75 border-solid rounded-2xl border border-gray-700 bg-gray-950 text-xl p-4 mt-6">
+            <div className="flex space-x-6">
+              <Link href="/" className={`hover:text-orange-400 ${pathname === '/' ? 'text-orange-500' : ''}`}><FaHouseUser /></Link>
+              <Link href="/services" className={`hover:text-orange-400 ${pathname === '/services' ? 'text-orange-500' : ''}`}><MdEngineering /></Link>
+              <Link href="/portfolio" className={`hover:text-orange-400 ${pathname === '/portfolio' ? 'text-orange-500' : ''}`}><MdCollectionsBookmark /></Link>
+              <Link href="/about" className={`hover:text-orange-400 ${pathname === '/about' ? 'text-orange-500' : ''}`}><FaCircleInfo /></Link>
+              <Link href="/contact" className={`hover:text-orange-400 ${pathname === '/contact' ? 'text-orange-500' : ''}`}><FaPhone /></Link>
+            </div>
+          </button>
+        </div>
+      </div>
     </>
   )
 }
