60ec10d93d8bd2a528f9c2802a663b86b33237c8
John <johntaykimani@gmail.com>
2024-07-20 04:47:31 +0300

    feat: Heading added on contact page.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,20 @@ export default function Contact() {
   return (
     <>
     <Navbar/>
-    <h1 className="text-4xl text-center pt-4 font-bold">Contact Me Page</h1>
+
+    {/* Page Heading */}
+
+    <div className="relative">
+      <div className="absolute inset-0 flex justify-center items-center">
+        <h1 style={{ fontSize: '9rem' }} className="uppercase font-extrabold text-stone-950">Contact</h1>
+      </div>
+      <div className="m-24 relative z-10">
+        <h1 className="text-4xl text-center font-bold">
+          Hey, let's <span className="text-orange-500">connect</span>.
+        </h1>
+        <hr className="w-1/12 h-0.5 mx-auto mt-2 bg-gradient-to-r from-gray-500 via-orange-500 to-gray-500 border-0"/>
+      </div>
+    </div>
     </>
   )
 }
