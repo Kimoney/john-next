ec5167359cceff25b924fec6f508d0e0d325918c
John <johntaykimani@gmail.com>
2024-07-22 13:58:28 +0300

    style: Adjusted font color for page headings.

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -9,11 +9,11 @@ export default function Portfolio() {
 
     <div className="relative">
         <div className="absolute inset-0 flex justify-center items-center">
-          <h1 className="uppercase font-extrabold text-stone-950 whitespace-nowrap text-7xl sm:text-7xl md:text-9xl lg:text-[9rem] xl:text-[9rem]">Portfolio</h1>
+          <h1 className="uppercase font-extrabold text-stone-800 whitespace-nowrap text-7xl sm:text-7xl md:text-9xl lg:text-[9rem] xl:text-[9rem]">Portfolio</h1>
         </div>
         <div className="m-4 sm:m-8 md:m-16 lg:m-24 relative z-10">
           <h1 className="text-2xl sm:text-2xl md:text-4xl lg:text-5xl text-center font-bold">
-            My <span className="text-orange-500">Experiences</span>
+            What I Have <span className="text-orange-500">Done</span>.
           </h1>
           <hr className="w-1/6 sm:w-1/6 md:w-1/8 lg:w-1/12 h-0.5 mx-auto mt-1 bg-gradient-to-r from-gray-500 via-orange-500 to-gray-500 border-0"/>
         </div>
